d7c8030ee1deff60fe027983d16547277f8af2af
Sajid Ali Khan <sajid.hmdrd@gmail.com>
2023-01-17 13:14:14 +0530

    new files added

---
+++ 02-features.js
@@ -0,0 +1,40 @@
+JavaScript Features
+
+What is  High level language?
+    High level programming language vah programming language hoti hai jise is trah se design kia gya hai ki us language ko asani se likha, pdha vah smja ja skta hai. High level language ek english like nature language ke equal hi hoti hai. High level language ko sikhna asan hota hai. High level language kafi sare actions aise hote hain jo vo khud manage kr leti hai aur hume ni krne hote hain. for example :
+    1) memory managemant.
+    2) communication with hardware
+
+What is Garbage Collected ?
+    Javascript ek garbage collected language hai. Garbage collected ka yha ye matlab hai ki Javascript engine me ek utility chalti hai jise garbage collector khte hain. Garbage collector unused memory ko free karta hai. yah automatically run hota hai. Garbage collector aise objects ko identify krta hai jinko program me ab access ni kia ja skta un objects ko memory se remove kr deta hai.
+    
+    Memory size agar kam ho to garbage collector slow run hoga, aur agar memory size kam hai to fast run hoga.
+
+What is Interpreted Language (JIT)-> (Just in time commpiler) ?
+    compiler: yeh ak utility hai jo kisi bhi code ko compile karti hai. Vah compile karne ka bad ek new file generate karta hai vah us file ko run karta hai.
+    Interpreter: Yeh ek utility hoti hai jo Java script ke code ko line by line run karti hai.
+    JIT: JavaScript compiler and Interpreter dono ka mixture hai. Yeh ek technique hai jise ki JavaScript engine ke dwara code ki performance improve krne ke lie kia jata hai. Code ko runtime pr compile krke uski performance ko improve krta hai. JIT vah code jo jyada use ho rha hai us code ka optimized machine code generate krta hai. Isse JavaScript ki performance kafi imporve hoti hai kuki optimized code original Interpreted JavaScript code se fast run hota hai.  
+
+What is Multi Paradigm ?
+    Multi Paradigm ka matlb hai ki javascript different style of programming support karti hai. for example :
+    1) Object orinted programming
+    2) functional programming
+    3) event driven programming
+
+What Prototype based functions ?
+What is First Class Function ?
+    Advanced topic. Will be covered in later files in detail.
+
+What is Dynamically Typee/ Dynamic ?
+   Javascript ek Dynamically based language hai.iska mtlb yaha hai ki hum kisi variable me kisi b type ka data assign kar skte hain. Jis b type ka data hm variable me assign krte hain variable ki vahi type ho jati hai.
+
+for example:
+    let x = 10; // type will be number
+    x = "wecode"; //type will be string
+    x = false; //type will be boolean
+
+What is Single Threaded ?
+    JavaScript ek single threaded language hai. Thread ek process ka part hota hai. Jo ki independently within process run hota hai. Javascript ek sath ek sa jyada thread run nahi kar sakti iska mtlb hai ki javascript engine me kisi bhi time par ek hi thread run hogi.
+
+ What is Non-Blocking Event Loop ?
+     advanced topic
---
+++ 03-value_variable_datatype.js
@@ -0,0 +1,80 @@
+What is Data type:
+  Data type kisi bhi data ki type btata hai ki vah data kis type ka hai.data tyoe two type ka hota hai. Data types 2 type ki hoti hain:
+    primitive data type
+    non-primitive data type
+
+primitive data type(inbuilt data type ):
+  Primitive data type vah data type hoti hai jo kisi bhi programming language me pre define data type hoti hai.  
+          
+  1)number: Javascript me kisi bhi number ka liye number data type use hota hai.
+  2)String: Javascript me kisi bhi word, text, ya paragraph/sentence ka liye string data type ka use hota hai.
+  3)undefined: kisi bhi variable ko koi bhi data nhi assign kiya hai to us variable 
+  ki data type undefined hogi
+  4)boolean: boolean data type vah data store krane jiski value true/false ho ke liye use kia jata hai. true aur false ko 1 and 0 se b represent krte hain. 
+  5)BigInt: BigInt vah data type hota hai jo bahut big number ka leya use hota hai. Aise numbers jo number datatype me store ni hote hain. 
+
+
+Non primitve (reference types) data type: Non primitive data type ko reference data types b kha jata hai. Ye data types primitive data type se hi bni hoti hai. But in Non primitive data type ko user khud define karta hai. Non primitve data type me hum difrent type ka data store kar saktay hai. 
+  1) object
+  2) array
+  3) functions
+  4) Dates
+  5) Regular expressions
+  6) Maps 
+  7) Sets
+  8) WeakMap
+
+
+
+
+value and variable
+value vah data type hoti hain.jise hum kisi variable me stoe krate  hain.
+
+variable ak identyfire hota hain.jise ke hum alg alg trha ke value store krane ke leya 
+use karte hain.
+let x = 10;
+//10 is value and x is variable
+//variable can hold a value
+
+//==========Data Types======================
+
+//==========Primitive Data Types========================
+
+//Primitive Data Types
+
+//Number
+let value = 10;
+
+//Boolean
+let flag = false;
+
+//String
+let str = "WeCode";
+
+//undefined
+let val; //value will be undefined here
+val = undefined; //we can also assign undefined to a variable
+
+//Symbol
+let sym = Symbol("A");
+
+//BigInt
+let num = BigInt(10);
+num = 10n;
+
+//===============Non Primitive Data Types=======================
+
+//Object
+let obj = {
+  myName: "Wecode",
+  age: 21,
+};
+
+//Array
+let arr = [1, 2, 3, 4, 5];
+
+/*
+
+
+
+*/
