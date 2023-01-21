d89798ad4d7feec12264e0519f85a486e0baf0bd
Sajid Ali Khan <sajid.hmdrd@gmail.com>
2023-01-21 21:39:18 +0530

    Added github notes

---
+++ notes/script.js
@@ -0,0 +1,12 @@
+difference between Declaration and initialisation 
+What is GitHub. Basic introduction
+What is Memory, What is RAM, ROM
+In which memory JavaScript code runs ? 
+Understanding memory size terms
+Basic understanding of Developer tool
+Topic from syllabus if time permits
+document.write ka practical
+alert ka practical
+koi b code likhna hai aur usme single line and multi comment ka use krna hai 
+Single line comment me code ke bare me likhna and multi line comment me code ke bare
+me likhna hai plus @author me tumara naam likhna and @date me us din ki date likhni hai
