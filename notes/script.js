c49bf31edb02dc0e8dbc03b78ca4752468e9000e
Sajid Khan <sajid.hmdrd@gmail.com>
2023-02-11 01:02:44 +0530

    Readme changes

--- notes/script.js
+++ notes/script.js
@@ -1,26 +1,6 @@
-let str = "Wecode";
-console.log(str.repeat(10));
-console.log(str.toUpperCase());
-console.log(str.toLowerCase());
-console.log(str.startsWith("We", 3)); //case sensitive
-console.log(str.indexOf("c"));
-console.log(str.lastIndexOf("c"));
-console.log(str.includes("cod"));
-console.log(str.charAt(4)); // d
-console.log(str.charAt(10)); // check length otherwise answer will be empty
-console.log(str.replace("cod", "hello")); //replace function will not change original string
-console.log("         Sajid Khan                 ".trim());
-console.log("         Sajid Khan    ".trimStart());
-console.log("         Sajid Khan    ".trimEnd());
-console.log(str.concat(" Jaipur", " Rajasthan", " India"));
-if (str.indexOf("cod") > -1) {
-  console.log("yes");
-} else {
-  console.log("no");
+let i, j;
+for (let i = 1, j = 10; i <= 10; i++) {
+  j--;
+  console.log(i, j);
 }
-
-if (str.includes("cod")) {
-  console.log("yes");
-} else {
-  console.log("no");
-}
+console.log(i, j);
